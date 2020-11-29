4a7376987d70e442a684f64cf601dd17747f826a
KozhevnikovAlexandr <56560126+KozhevnikovAlexandr@users.noreply.github.com>
2020-11-29 20:10:03 +0500

    Add files via upload

--- TimedAutomaton.py
+++ TimedAutomaton.py
@@ -1,21 +1,53 @@
 from Transition import *
 
-class TimedAutomaton():
+class TimedAutomaton:
     transitions = []
     states = []
+    current_state = []
 
     def __init__(self, alphabet):
         self.alphabet = alphabet
 
     def create_new_state(self, state):
         self.states.append(state)
+        if len(self.current_state) == 0:
+            self.current_state = self.states[-1]
         return self.states[-1]
 
-    def create_new_transition(self, source, trigger, destination):
-        self.transitions.append(Transition(source, trigger, destination))
+    def create_new_transition(self, source, time, destination):
+        self.transitions.append(Transition(source, time, destination))
 
     def find_transition(self, start, end):
         for i in self.transitions:
-            if i.source == start and i.destination == end:
+            if i.source == start and end == i.destination:
                 return i
         return False
+
+    def find_nearest_transition(self, input):
+        for i in self.transitions:
+            if i.source == self.current_state and i.destination[0] == input:
+                return i
+        return False
+
+    def find_transition_with_given_time(self, start, end, time):
+        for i in self.transitions:
+            if i.source == start and i.destination == end and time == i.time:
+                return i
+        return False
+
+    def make_transition(self, start, end):
+        for i in self.transitions:
+            if i.source == start and end == i.destination:
+                return i.destination
+        return False
+
+    def adapt_timing_information(self, transition, time):
+        if time < transition.time:
+            transition.time = time
+            return True
+        return False
+
+    def get_result(self, input):
+        a = self.find_nearest_transition(input)
+        self.current_state = a.destination
+        return self.current_state[1]
--- main.py
+++ main.py
@@ -1,29 +1,80 @@
-import random
+import json
 from TimedAutomaton import TimedAutomaton
-from test_data import Test_data
-from Transition import Transition
+from TestData import Test_data
+from ia import Simplest_automaton
+import random
 
-ta = TimedAutomaton([1, 2, 3, 4, 5])
-data = [Test_data([random.randrange(1, 6) for i in range(5)],
-                  [random.randrange(0, 2) for j in range(5)], random.randrange(1, 11))
-        for x in range(1000)]
-currentState = data[0].configuration
+convergence_number = 100
+
+ta = TimedAutomaton([0, 1])
+investigated_automaton = Simplest_automaton()
+
+
+data1 = [[investigated_automaton.make_random_transition(), investigated_automaton.current_state] for x in range(100000)]
+
+with open('input.json', 'r') as read_file:
+    input = json.load(read_file)
+
+with open('output.json', 'r') as read_file:
+    output = json.load(read_file)
+
+work_input = input[:500]
+work_output = output[:500]
+
+data = [list(tup) for tup in zip(work_input, work_output)]
+test_data = [['a', 3], ['b', 2], ['b', 2], ['a', 3], ['a', 3]]
+
+currentState = ta.create_new_state(data[0])
 stateExist = False
+changes = 0
+n_conv = 0
+state_count = 1
+transition_count = 0
+observation_count = 0
+time_was_changed = False
+time = 0
 
-for new_observation in data:
+for new_observation in data1:
+    observation_count += 1
     for state in ta.states:
-        if new_observation.configuration == state:
+        if new_observation == state:
             stateExist = True
-            transition = ta.find_transition(currentState, new_observation.configuration)
+            transition = ta.find_transition(currentState, state)
             if transition:
-                'a.adapt_timing_information(transition, i.time)'
+                time_was_changed = ta.adapt_timing_information(transition, time)
             else:
-                ta.create_new_transition(currentState, new_observation.event, state)
-        currentState = state
+                ta.create_new_transition(currentState, time, state)
+            currentState = state
     if not stateExist:
-        s_new = ta.create_new_state(new_observation.configuration)
-        ta.create_new_transition(currentState, new_observation.event, s_new)
+        s_new = ta.create_new_state(new_observation)
+        ta.create_new_transition(currentState, time, s_new)
         currentState = s_new
-    'if ta.learningConverged():'
-print(len(ta.states))
-print(len(ta.transitions))
+    stateExist = False
+    if n_conv > convergence_number:
+        break
+    if transition_count == len(ta.transitions) and state_count == len(ta.states) and not time_was_changed:
+        n_conv += 1
+    else:
+        n_conv = 0
+    time_was_changed = False
+    state_count = len(ta.states)
+    transition_count = len(ta.transitions)
+
+print('Изучено данных: '+ str(observation_count))
+print('Состояний: ' + str(len(ta.states)))
+print('Переходов: ' + str(len(ta.transitions)))
+print('*' * 10 + 'TEST' + '*' * 10)
+test_input = input[500:]
+test_output = output[500:]
+count = 0
+q = 0
+
+for i in range(len(test_input)):
+    a = ta.get_result(test_input[i])
+    if a == test_output[i]:
+        count += 1
+    else:
+        q += 1
+print('верно предсказано: '+ str(count))
+print('всего проверок: ' + str(1000 - observation_count))
+print('процент успеха: ' + str(count/(1000 - observation_count)*100))
