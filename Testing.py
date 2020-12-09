bd860e5d67e76b5ff008e92278afeb9ddeb80112
KozhevnikovAlexandr <56560126+KozhevnikovAlexandr@users.noreply.github.com>
2020-12-09 23:46:06 +0500

    Add files via upload

--- Testing.py
+++ Testing.py
@@ -16,28 +16,15 @@ def checking_transitions(ta, data):
                 break
         if success_count == previous_success_count:
             anomalies += 1
+        state = new_state
     return [success_count, anomalies]
-
-
-def cheching_result(ta, data):
-    success_count = 0
-    anomalies = 0
-    current_state = data[0]
-    for new_state in data:
-        ps = success_count
-        for transition in ta.transitions:
-            if transition.source == current_state and transition.destination[0] == new_state[0] and \
-                    transition.destination[1] == new_state[1]:
-                success_count += 1
-                current_s   tate = new_state
-                break
-        if success_count == ps:
-            anomalies += 1
-        return [success_count, anomalies]
 
 
 if __name__ == '__main__':
     ta = TimedAutomaton([0, 1])
     data = [test_data_constructor.state_to_dec(x) for x in test_data_constructor.get_data()]
     ta = training(ta, data, convergence_number=100)
-    print(checking_transitions(ta, data))
+    result = checking_transitions(ta, data)
+    print('успехов ' + str(result[0]))
+    print('аномалий ' + str(result[1]))
+    print('процент успеха ' + str(result[0] / len(data) * 100))
